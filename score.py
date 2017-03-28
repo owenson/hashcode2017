f1aaf0bbffc709975e986b40480f230ef475a9d0
Gareth Owenson <drgowen@gmail.com>
2017-03-28 15:08:32 +0100

    added command line scoring via score.py

--- score.py
+++ score.py
@@ -13,6 +13,30 @@ def score(endpoints):
 
     return int(score*1000.0/totalReqs)
 
+#if you want to use on the command line
+if __name__ == "__main__":
+    if len(sys.argv)!=3:
+        print "python score.py problemFile solutionfile"
+        sys.exit(1)
+    print "Calculating score - note little validation is done on your input file so beware"
+#reads in problem file and generates the classes
+    from models import *
+
+    for r in requests:
+        endpoints[r.epid].addRequests(r)
+
+#read in solution file ready for the score() function
+    f = open(sys.argv[2], "r")
+    f.readline() # skip count
+    for _l in f.readlines():
+        l = map(int,_l.split(" "))
+        for vid in l[1:]:
+            caches[l[0]].addVideo(vid)
+    print "SCORE: ",score(endpoints)
+
+
+
+
 # output:
 # ncache servers
 # cservid video1 video2 etc
